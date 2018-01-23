b59b07f161fbde3737edaa6ead2b84c265e65419
sofiaknezevic <sofia.knezevic@gmail.com>
2018-01-23 14:25:40 -0800

    Add necessary hint to challenge in Variable and Types

--- W4D1_Playground.playground/Pages/Variable and Types.xcplaygroundpage/Contents.swift
+++ W4D1_Playground.playground/Pages/Variable and Types.xcplaygroundpage/Contents.swift
@@ -24,7 +24,8 @@ students = 7
  What happens if you try to change the value of a constant?
  */
 // variables & constants
-
+var name = "Sofia"
+let age = 23
 
 //: ---
 
@@ -36,7 +37,7 @@ students = 7
  - Experiment:
  Hold down the Option key and click on the variables `school` and `students` above to see a popup window with information about the variable. If we click on `school`, we can see it's a `String`. What type is `students`?
  */
-
+// Students is of type int
 /*:
  We can change the data stored in a variable using the *assignment operator* `=`. The new value must _always_ be the same type as the variable, you can't assign a new value with a different type.
  
@@ -47,6 +48,7 @@ students = 7
  */
 
 var weeksCompleted = 4
+weeksCompleted = 5
 
 /*:
  As you can see, you don't have to write the type explicitly. Providing a value when you create a constant or variable lets the compiler _infer_ its type. In the example above, the compiler infers that `students` is an integer because its initial value is an integer.
@@ -72,7 +74,7 @@ let explicitDouble: Double = 70
  - Experiment:
  Create a constant with an explicit type of `Float` and a value of `4`.
  */
-
+let constantFloat:Float = 4
 
 /*:
  - Experiment:
@@ -80,11 +82,11 @@ let explicitDouble: Double = 70
  */
 
 var tipAmount = 6.01
-// tipAmount's type is:
+// tipAmount's type is: Double
 
 
 var isOpen = true
-// isOpen's type is:
+// isOpen's type is: Boolean
 
 /*:
 Values are never implicitly converted to another type. If you need to convert a value to a different type, explicitly make an instance of the desired type.
@@ -98,10 +100,10 @@ let widthLabel = label + String(width)
  - Experiment:
  Try removing the conversion to `String` from the last line. What error do you get?
  */
-
+// binary operator cannot be applied to two different types
 /*:
  - Callout(Challenge):
- Let's solve some simple geometric math problems. For each question, make sure you show your work just like how your teachers told you in high school. Show all the variables you are using and give them meaningful names. Hint: You can delcare your own 'pi' using 3.14
+ Let's solve some simple geometric math problems. For each question, make sure you show your work just like how your teachers told you in high school. Show all the variables you are using and give them meaningful names. Hint: You can declare your own 'pi' using 3.14
  */
 
 /*:
@@ -112,22 +114,25 @@ let widthLabel = label + String(width)
  \
  Volume of cylinder = 'pi' * radius^2 * height
  */
+/*:
+ Hint: 10^2 does not equal 10*10 in Swift
+ */
 
 /*:
  - Callout(Problem 1):
  Find the area of a circle with a radius of 10 metres
  */
-
+let circleArea = (3.14 * (10*10))
 
 /*:
  - Callout(Problem 2):
  Find the circumference of a circle with a diameter of 24 metres. Hint: Radius is half the length of the diameter
  */
-
+let circleCircumference = (2*3.14*12)
 
 /*:
  - Callout(Problem 3):
  Find the volume of a cylinder with a radius of 5 metres and a height of 7 metres
  */
-
+let cylinderVolume = (3.14*(5*5)*7)
 //: [Next](@next)
