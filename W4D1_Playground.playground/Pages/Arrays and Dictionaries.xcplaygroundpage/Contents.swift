f68af76f012aec5c71858286ece7edaca89ed090
yongwoohuh <yongwoo.huh@me.com>
2018-02-05 14:36:16 -0500

    go through Arrays and Dicionarires section

--- W4D1_Playground.playground/Pages/Arrays and Dictionaries.xcplaygroundpage/Contents.swift
+++ W4D1_Playground.playground/Pages/Arrays and Dictionaries.xcplaygroundpage/Contents.swift
@@ -4,34 +4,44 @@
  
   Arrays and dictionaries allow us to store a collection of data. Create arrays and dictionaries using brackets '[]'. Remember that arrays hold a collection data based on an index, so we can access a value by writing the desired index within the brackets. Arrays also begin at index `0`. Dictionaries hold a key and a value, and we can access a value by writing the desired key within the brackets.
  */
-
-// We can declare an array with square brackets syntax
-var myNumberArray = [1,2,3]
-print("The value at index 1 is: \(myNumberArray[1])")
-
-// We can declare a dictionary with square brackets and key/value data
-var myDictionary = ["Students" : 5, "Teachers" : 2]
-
-// This is how we add a new key/value to our dictionary
-myDictionary["Principal"] = 1
+//
+//// We can declare an array with square brackets syntax
+//var myNumberArray = [1,2,3]
+//print("The value at index 1 is: \(myNumberArray[1])")
+//
+//// We can declare a dictionary with square brackets and key/value data
+//var myDictionary = ["Students" : 5, "Teachers" : 2]
+//
+//// This is how we add a new key/value to our dictionary
+//myDictionary["Principal"] = 1
 
 /*:
  - Experiment:
  Try creating an array and dictionary using `'let'` vs `'var'` then try adding something to them. What do you notice? For the array, you can use a method called 'append' to add something to it.
  */
+//let evenNumbers = [2, 4, 6, 8, 10]
+//var primeNumbers = [1, 3, 5, 7, 11, 13, 17, 19, 23]
+//
+//primeNumbers.append(29)
+//print("prime numbers \(primeNumbers)")
+
 
 
 /*:
  - Experiment:
  Try creating an empty array with an explicit type `[type]`. What happens when you add a value that isn't the same type?
  */
-
+//var myNums: [Int]
+//myNums = [2, 5, 6, 8, 54]
+//var myString = [String]()
+//myString.append("Life is short")
+//myString.append("Amor Fati")
 
 /*:
  - Experiment:
  Try creating an empty dictionary with an explicit type. `[keyType: valueType]`
  */
-
+//var myDic : [String: Int] = ["Yongwoo": 42]
 
 /*:
  - Experiment:
@@ -43,8 +53,18 @@ myDictionary["Principal"] = 1
  - reverse
  - count
  */
-
-
+//var squareNumbers = [1, 4, 9, 25, 36, 49, 64, 81, 100]
+//squareNumbers.append(121)
+//squareNumbers.insert(16, at: 0)
+//print(squareNumbers)
+//squareNumbers.remove(at: 0)
+//print(squareNumbers)
+//squareNumbers.removeAll()
+//
+//squareNumbers = [1, 4, 9, 25, 36, 49, 64, 81, 100]
+//squareNumbers.reverse()
+//squareNumbers.insert(16, at: 3)
+//squareNumbers.count
 /*:
  - Experiment:
  Create a dictionary with any values of your choice. Play around with accessing a value by its key. Try keys using different case sensitivity, what happens? Then try playing with the following methods of a dictionary and find out what they do:
@@ -52,7 +72,11 @@ myDictionary["Principal"] = 1
  - isEmpty
  - count
  */
-
+//var numbers = ["primes": [1, 2, 3, 5, 7, 11, 13], "squareNumbers": [1,4,9,16,25,36]]
+//numbers.removeValue(forKey: "squareNumbers")
+//print(numbers)
+//numbers.isEmpty
+//numbers.count
 
 /*:
  - Callout(Challenge):
@@ -60,7 +84,10 @@ myDictionary["Principal"] = 1
  
     Finally, print out each person's name from the array using their index and string interpolation
 */
-
+//var names : [String]
+//names = ["John", "Karen"]
+//names.append("Jane")
+//print("There is \(names[0]), \(names[1]), \(names[2]) in this array of names")
 
 /*:
  - Callout(Challenge):
@@ -74,6 +101,8 @@ myDictionary["Principal"] = 1
  
     On the next line, add this last person's info: Bob - $45000
  */
-
+var salaries = ["John": 50000, "Jane": 70000, "Karen": 62000,]
+salaries["Bob"] = 45000
+print(salaries)
 
 //: [Next](@next)
